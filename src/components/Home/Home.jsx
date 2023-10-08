89b9c389c2a902937f5eea072fca4430ab768de5
Flocky05 <md.rakibul.islam.0501@gmail.com>
2023-10-08 17:17:26 +0600

    added nursing section

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,6 +1,7 @@
 
 import img from "../../assets/images/Header/hero.jpg"
 import Computer from "../Computer/Computer";
+import Nurturing from "../Nurturing/Nurturing";
 import Security from "../Security/Security";
 const Home = () => {
     return (
@@ -12,6 +13,7 @@ const Home = () => {
             </div>
             <Security></Security>
             <Computer></Computer>
+            <Nurturing></Nurturing>
         </div>
     );
 };
---
+++ src/components/Nurturing/Nurturing.jsx
@@ -0,0 +1,98 @@
+
+import img from "../../assets/images/security/done-100.png"
+import img1 from "../../assets/images/Nursing/tik.png"
+const Nurturing = () => {
+    return (
+       <div>
+            <h2 className="text-3xl p-5 font-bold text-center">Nurturing a Solid Foundation</h2>
+             <div className="grid grid-cols-1 lg:grid-cols-3 gap-20 m-20">
+                <div className="bg-gray-300 rounded-lg p-20">
+                    <img className="mx-auto" src={img} alt="" />
+                    <h3 className="text-3xl font-bold text-center">Primary Curriculum</h3>
+                    <div className="flex gap-5 items-center">
+                        <img className="w-10" src={img1} alt="" />
+                        <p className="text-lg">Literacy & Language Skills</p>
+                    </div>
+                    <div className="flex gap-5 items-center">
+                        <img className="w-10" src={img1} alt="" />
+                        <p className="text-lg">Mathematics</p>
+                    </div>
+                    <div className="flex gap-5 items-center">
+                        <img className="w-10" src={img1} alt="" />
+                        <p className="text-lg">A regional language</p>
+                    </div>
+                    <div className="flex gap-5 items-center">
+                        <img className="w-10" src={img1} alt="" />
+                        <p className="text-lg">Environment Science</p>
+                    </div>
+                    <div className="flex gap-5 items-center">
+                        <img className="w-10" src={img1} alt="" />
+                        <p className="text-lg">Information and communication technology</p>
+                    </div>
+                    <div className="flex gap-5 items-center">
+                        <img className="w-10" src={img1} alt="" />
+                        <p className="text-lg">IPhysical education</p>
+                    </div>
+                </div>
+                <div className="bg-gray-300 rounded-lg p-20">
+                    <img className="mx-auto" src={img} alt="" />
+                    <h3 className="text-3xl font-bold text-center">Secondary Curriculum</h3>
+                    <div className="flex gap-5 items-center">
+                        <img className="w-10" src={img1} alt="" />
+                        <p className="text-lg">Literacy & Language Skills</p>
+                    </div>
+                    <div className="flex gap-5 items-center">
+                        <img className="w-10" src={img1} alt="" />
+                        <p className="text-lg">Mathematics</p>
+                    </div>
+                    <div className="flex gap-5 items-center">
+                        <img className="w-10" src={img1} alt="" />
+                        <p className="text-lg">A regional language</p>
+                    </div>
+                    <div className="flex gap-5 items-center">
+                        <img className="w-10" src={img1} alt="" />
+                        <p className="text-lg">Environment Science</p>
+                    </div>
+                    <div className="flex gap-5 items-center">
+                        <img className="w-10" src={img1} alt="" />
+                        <p className="text-lg">Information and communication technology</p>
+                    </div>
+                    <div className="flex gap-5 items-center">
+                        <img className="w-10" src={img1} alt="" />
+                        <p className="text-lg">IPhysical education</p>
+                    </div>
+                </div>
+                <div className="bg-gray-300 rounded-lg p-20">
+                    <img className="mx-auto" src={img} alt="" />
+                    <h3 className="text-3xl font-bold text-center">EYP Curriculum</h3>
+                    <div className="flex gap-5 items-center">
+                        <img className="w-10" src={img1} alt="" />
+                        <p className="text-lg">Conceptual Learning</p>
+                    </div>
+                    <div className="flex gap-5 items-center">
+                        <img className="w-10" src={img1} alt="" />
+                        <p className="text-lg">Literacy and Numeracy</p>
+                    </div>
+                    <div className="flex gap-5 items-center">
+                        <img className="w-10" src={img1} alt="" />
+                        <p className="text-lg">Kinesthetic Learning & Motor Skill Development</p>
+                    </div>
+                    <div className="flex gap-5 items-center">
+                        <img className="w-10" src={img1} alt="" />
+                        <p className="text-lg">Experiential Learning</p>
+                    </div>
+                    <div className="flex gap-5 items-center">
+                        <img className="w-10" src={img1} alt="" />
+                        <p className="text-lg">Information and communication technology</p>
+                    </div>
+                    <div className="flex gap-5 items-center">
+                        <img className="w-10" src={img1} alt="" />
+                        <p className="text-lg">Environmental Awareness</p>
+                    </div>
+                </div>
+            </div>
+       </div>
+    );
+};
+
+export default Nurturing;
