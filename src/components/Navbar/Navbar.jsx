c5ed7109003da97d4562911a8266766117c386d3
Flocky05 <md.rakibul.islam.0501@gmail.com>
2023-10-08 09:28:23 +0600

    update Title section

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,9 +1,21 @@
-
-
+import { Link } from "react-router-dom";
+import img from "../../assets/images/logo/logo.png"
 const Navbar = () => {
     return (
         <div>
-            <h3>This is navbar section</h3>
+            <div>
+                <Link to="/"><img src={img} alt="" /></Link>
+            </div>
+            <div>
+                <Link to="/">Home</Link>
+                <Link to="/who">Who We are?</Link>
+                <Link to="/admission">Admission</Link>
+                <Link to="/careers">Careers</Link>
+                <Link to="/school-policies">School  Policies</Link>
+                <Link to="/infrastructure">Infrastructure</Link>
+                <Link to="/infrastructure">Infrastructure</Link>
+
+            </div>
         </div>
     );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -6,11 +6,22 @@ import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
+import Main from './components/Main/Main';
+import Home from './components/Home/Home';
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello world!</div>,
+    element: <Main></Main>,
+    children:[
+      {
+        path:"/",
+        element:<Home></Home>
+      },
+      {
+        
+      }
+    ]
   },
 ]);
 ReactDOM.createRoot(document.getElementById('root')).render(
