bbec54d94de297825a5b78baa62ab74a9fd04836
Flocky05 <md.rakibul.islam.0501@gmail.com>
2023-10-08 16:26:37 +0600

    added security section

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,5 +1,6 @@
 
 import img from "../../assets/images/Header/hero.jpg"
+import Security from "../Security/Security";
 const Home = () => {
     return (
         <div className="relative">
@@ -8,6 +9,7 @@ const Home = () => {
                 <h2 className="text-4xl font-bold text-center p-4 text-red-600">Bhuapur Govt Pilot Hight School</h2>
                 <p className="text-center text-lg">Bhuapur Pilot Hight School is an ducational establishment that is located at Bhuapur,Tangail. Its Educational Institute Identification Number or EIIN, is 114019. On 20 January,1941, it was first put into operation. The alternative name for Bhuapur Pilot Hight School. It is a Combined sort of co-educational program. The institution provides education in the following fields: Business Studies, Science, Humanities.</p>
             </div>
+            <Security></Security>
         </div>
     );
 };
---
+++ src/components/Security/Security.jsx
@@ -0,0 +1,34 @@
+
+import img from "../../assets/images/security/done-100.png"
+import img1 from "../../assets/images/security/career.webp"
+import img2 from "../../assets/images/security/schoolBus.jpg"
+import img3 from "../../assets/images/security/team.webp"
+import img4 from "../../assets/images/security/cc camera.jpg"
+
+const Security = () => {
+    return (
+        <div className="bg-black p-20 text-white">
+            <h2 className="text-4xl p-10 font-bold text-center">Orchids'5 Petals of Purpose</h2>
+            <div className="lg:flex gap-20">
+                <div className="grid grid-cols-1 lg:flex justify-between items-center p-10 border-2 rounded-lg m-10">
+                    <div>
+                        <img className="invert w-20" src={img} alt="" />
+                        <h2 className="text-3xl font-bold text-white">A Fortress of Care</h2>
+                        <p className="text-lg">There’s Zero Compromise when it comes to your Child’s Safety.</p>
+                    </div>
+                    <div className="flex flex-col relative">
+                        <img className="p-2" src={img1} alt="" />
+                        <img className="p-2  " src={img3} alt="" />
+                    </div>
+                </div>
+                <div className="border-2 rounded-lg p-20 m-10">
+                    <img className="" src={img2} alt="" />
+                    <h2 className="text-xl">Learn More</h2>
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officiis sed est deserunt cumque quas atque nihil, in laudantium. Harum, dolores.</p>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Security;
