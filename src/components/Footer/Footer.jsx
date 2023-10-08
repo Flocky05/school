51f72c724571de033e41eb291a1fa32affdd7eb5
Flocky05 <md.rakibul.islam.0501@gmail.com>
2023-10-08 13:50:45 +0600

    added home section

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,10 +1,78 @@
 
-
+import { Link } from 'react-router-dom';
+import logo from '../../assets/images/logo/logo.png';
+import img1 from "../../assets/images/footerIcons/facebook-64.png"
+import img2 from "../../assets/images/footerIcons/instagram-64.png"
+import img3 from "../../assets/images/footerIcons/linkedin-64.png"
+import img4 from "../../assets/images/footerIcons/twitter-64.png"
 const Footer = () => {
     return (
-        <div>
-           <h3>This is Footer section</h3> 
-        </div>
+        <footer className="px-4 divide-y-2 bg-gray-200 ">
+            <div className="container flex flex-col justify-between py-10 mx-auto space-y-8 lg:flex-row lg:space-y-0">
+                <div className="lg:w-1/3">
+                    <Link rel="noopener noreferrer" to="/" className="flex justify-center space-x-3 lg:justify-start">
+                        <div className="flex items-center justify-center rounded-full">
+                            <img className='w-12' src={logo} alt="" />
+                        </div>
+                        <span className="self-center text-2xl font-semibold text-orange-500 font-Berkshire">BGPHS</span>
+                    </Link>
+                </div>
+                <div className="grid grid-cols-2 text-sm gap-x-3 gap-y-8 lg:w-2/3 sm:grid-cols-4">
+                    <div className="space-y-3">
+                        <h3 className="tracking-wide uppercas">School Event</h3>
+                        <ul className="space-y-1">
+                            <li>
+                                <Link rel="noopener noreferrer" to="/">Contest</Link>
+                            </li>
+                            <li>
+                                <Link rel="noopener noreferrer" to="/">Sports</Link>
+                            </li>
+                            <li>
+                                <Link rel="noopener noreferrer" to="/">Cultural function</Link>
+                            </li>
+                            <li>
+                                <Link rel="noopener noreferrer" to="/">Toure</Link>
+                            </li>
+                        </ul>
+                    </div>
+                    <div className="space-y-3">
+                        <h3 className="tracking-wide uppercase ">Govt. School</h3>
+                        <ul className="space-y-1">
+                            <li>
+                                <Link rel="noopener noreferrer" to="/">Privacy</Link>
+                            </li>
+                            <li>
+                                <Link rel="noopener noreferrer" to="/">Terms of Service</Link>
+                            </li>
+                        </ul>
+                    </div>
+                    <div className="space-y-3">
+                        <h3 className="uppercase">Developers</h3>
+                        <ul className="space-y-1">
+                            <li>
+                                <Link rel="noopener noreferrer" href="#">Public API</Link>
+                            </li>
+                            <li>
+                                <Link rel="noopener noreferrer" href="#">Documentation</Link>
+                            </li>
+                            <li>
+                                <Link rel="noopener noreferrer" href="#">Guides</Link>
+                            </li>
+                        </ul>
+                    </div>
+                    <div className="space-y-3">
+                        <div className="uppercase">Social media</div>
+                        <div className='flex gap-4'>
+                            <Link to="/"><img className='w-10' src={img1} alt="" /></Link>
+                            <Link to="/"><img className='w-10' src={img2} alt="" /></Link>
+                            <Link to="/"><img className='w-10' src={img3} alt="" /></Link>
+                            <Link to="/"><img className='w-10' src={img4} alt="" /></Link>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="py-6 text-sm text-center dark:text-gray-500">© 2022 Company Co. All rights reserved.</div>
+        </footer>
     );
 };
 
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,9 +1,13 @@
 
-
+import img from "../../assets/images/Header/hero.jpg"
 const Home = () => {
     return (
-        <div>
-            <h2>This is Home page</h2>
+        <div className="relative">
+            <img className="w-full h-screen" src={img} alt="" />
+            <div className="absolute -mt-72">
+                <h2 className="text-4xl font-bold text-center p-4 text-red-600">Bhuapur Govt Pilot Hight School</h2>
+                <p className="text-center text-lg">Bhuapur Pilot Hight School is an ducational establishment that is located at Bhuapur,Tangail. Its Educational Institute Identification Number or EIIN, is 114019. On 20 January,1941, it was first put into operation. The alternative name for Bhuapur Pilot Hight School. It is a Combined sort of co-educational program. The institution provides education in the following fields: Business Studies, Science, Humanities.</p>
+            </div>
         </div>
     );
 };
