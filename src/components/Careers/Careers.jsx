713370f2a2e2c1c4594c69caa040c8bdc45e17af
Flocky05 <md.rakibul.islam.0501@gmail.com>
2023-10-09 10:09:45 +0600

    responsive careers section

--- src/components/Careers/Careers.jsx
+++ src/components/Careers/Careers.jsx
@@ -13,15 +13,15 @@ const Careers = () => {
         superLargeDesktop: {
           // the naming can be any, depends on you.
           breakpoint: { max: 4000, min: 3000 },
-          items: 5
+          items: 3
         },
         desktop: {
           breakpoint: { max: 3000, min: 1024 },
-          items: 3
+          items: 2
         },
         tablet: {
           breakpoint: { max: 1024, min: 464 },
-          items: 2
+          items: 1
         },
         mobile: {
           breakpoint: { max: 464, min: 0 },
@@ -31,13 +31,13 @@ const Careers = () => {
     return (
         <div>
             <Carousel responsive={responsive}>
-                <div><img src={img1} alt="" /></div>
-                <div><img src={img2} alt="" /></div>
-                <div><img src={img3} alt="" /></div>
-                <div><img src={img4} alt="" /></div> 
-                <div><img src={img5} alt="" /></div> 
-                <div><img src={img6} alt="" /></div> 
-                <div><img src={img7} alt="" /></div> 
+                <div><img className="w-full object-cover" src={img1} alt="" /></div>
+                <div><img className="w-full object-cover" src={img2} alt="" /></div>
+                <div><img className="w-full object-cover" src={img3} alt="" /></div>
+                <div><img className="w-full object-cover" src={img4} alt="" /></div> 
+                <div><img className="w-full object-cover" src={img5} alt="" /></div> 
+                <div><img className="w-full object-cover" src={img6} alt="" /></div> 
+                <div><img className="w-full object-cover" src={img7} alt="" /></div> 
             </Carousel>;
         </div>
     );
