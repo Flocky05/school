e4d9d651ed4b48aefa904bc36993c4164dde5c77
Flocky05 <md.rakibul.islam.0501@gmail.com>
2023-10-09 10:02:55 +0600

    responsive Constituents section

--- src/components/CountryCampus/CountryCampus.jsx
+++ src/components/CountryCampus/CountryCampus.jsx
@@ -11,15 +11,15 @@ const CountryCampus = () => {
     return (
        <div>
             <h2 className="text-center p-4 text-3xl font-bold">BGPHS Constituents</h2>
-            <div className="grid grid-cols-4 gap-4 m-10">
-                <img className="w-full h-64 hover:" src={img1} alt="" />
-                <img className="w-full h-64" src={img2} alt="" />
-                <img className="w-full h-64" src={img3} alt="" />
-                <img className="w-full h-64" src={img4} alt="" />
-                <img className="w-full h-64" src={img5} alt="" />
-                <img className="w-full h-64" src={img6} alt="" />
-                <img className="w-full h-64" src={img7} alt="" />
-                <img className="w-full h-64" src={img8} alt="" />
+            <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-4 m-10">
+                <img className="w-full h-64 object-cover" src={img1} alt="" />
+                <img className="w-full h-64 object-cover" src={img2} alt="" />
+                <img className="w-full h-64 object-cover" src={img3} alt="" />
+                <img className="w-full h-64 object-cover" src={img4} alt="" />
+                <img className="w-full h-64 object-cover" src={img5} alt="" />
+                <img className="w-full h-64 object-cover" src={img6} alt="" />
+                <img className="w-full h-64 object-cover" src={img7} alt="" />
+                <img className="w-full h-64 object-cover" src={img8} alt="" />
             </div>
        </div>
     );
